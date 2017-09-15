ccda273fd7f62f5c46deb45c5a3ada5ed673b777
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-09-15 11:39:51 -0300

    Base solution created with empty projects.

---
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VehicleTracking.Web.API.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VehicleTracking.Web.API.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78cd1d61-895a-410a-8ace-3331c8ca4b5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
