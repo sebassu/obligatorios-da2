59a25fd18f5af9999acbc888ebc11b8d5deeb223
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-09-15 15:01:06 -0300

    [Red][User][FirstName]: A set of new unit tests was created, regarding the "FirstName" property in class "User".

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/UserTests.cs
@@ -0,0 +1,74 @@
+﻿using Domain;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Domain.Tests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class UserTests
+    {
+        private static User testingUser;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            testingUser = User.InstanceForTestingPurposes();
+        }
+
+        [TestMethod]
+        public void UserForTestingPurposesTest()
+        {
+            Assert.AreEqual("Usuario", testingUser.FirstName);
+        }
+
+        [TestMethod]
+        public void UserSetValidFirstNameTest()
+        {
+            testingUser.FirstName = "Mario";
+            Assert.AreEqual("Mario", testingUser.FirstName);
+        }
+
+        [TestMethod]
+        public void UserSetValidFirstNameCompoundTest()
+        {
+            testingUser.FirstName = "  Juan Martín  ";
+            Assert.AreEqual("Juan Martín", testingUser.FirstName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void UserSetInvalidFirstNameNumbersTest()
+        {
+            testingUser.FirstName = "1234";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void UserSetInvalidFirstNamePunctuationTest()
+        {
+            testingUser.FirstName = "!@.$#% *-/";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void UserSetInvalidFirstNameOnlySpacesTest()
+        {
+            testingUser.FirstName = " \n\n  \t\t \n\t  ";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void UserSetInvalidFirstNameEmptyTest()
+        {
+            testingUser.FirstName = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void UserSetInvalidFirstNameNullTest()
+        {
+            testingUser.FirstName = null;
+        }
+    }
+}
