334d9e3379731741d6497b5fc049a20fb716f351
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-09-16 16:15:16 -0300

    [Red][UserRepository][AddNewUser]: A set of new unit tests was created, regarding the "AddNewUser" method in class "UserRepository".

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Persistence tests/UserRepositoryTests.cs
@@ -0,0 +1,119 @@
+﻿using Domain;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Persistence.Tests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class UserRepositoryTests
+    {
+        [AssemblyInitialize]
+        public static void AssemblySetup(TestContext context)
+        {
+            DeleteAllDatabaseData();
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblySetup()
+        {
+            DeleteAllDatabaseData();
+        }
+
+        private static void DeleteAllDatabaseData()
+        {
+            using (var context = new VTSystemContext())
+            {
+                context.DeleteAllData();
+            }
+        }
+
+        [TestMethod]
+        public void URepositoryAddNewUserValidTest()
+        {
+            User userToVerify = User.CreateNewUser(UserRoles.TRANSPORTER, "Pablo", "Lamponne",
+                "primerLamponne", "NoHaceFaltaSaleSolo", "099212121");
+            UserRepository.AddNewUser(UserRoles.TRANSPORTER, "Pablo", "Lamponne",
+                "primerLamponne", "NoHaceFaltaSaleSolo", "099212121");
+            CollectionAssert.Contains(UserRepository.Elements.ToList(), userToVerify);
+        }
+
+        [TestMethod]
+        public void URepositoryAddNewUserReturnsAddedUserValidTest()
+        {
+            User addedUser = UserRepository.AddNewUser(UserRoles.TRANSPORTER, "Pablo", "Lamponne",
+                "segundoLamponne", "NoHaceFaltaSaleSolo", "099212121");
+            CollectionAssert.Contains(UserRepository.Elements.ToList(), addedUser);
+        }
+
+        [TestMethod]
+        public void URepositoryAddNewUserOnlyUsernamesMatchValidTest()
+        {
+            User userToVerify = User.InstanceForTestingPurposes();
+            userToVerify.Username = "tercerLamponne";
+            UserRepository.AddNewUser(UserRoles.TRANSPORTER, "Pablo", "Lamponne",
+                "tercerLamponne", "NoHaceFaltaSaleSolo", "099212121");
+            CollectionAssert.Contains(UserRepository.Elements.ToList(), userToVerify);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void URepositoryAddRepeatedUserInvalidTest()
+        {
+            UserRepository.AddNewUser(UserRoles.TRANSPORTER, "Pablo", "Lamponne",
+                "cuartoLamponne", "NoHaceFaltaSaleSolo", "099212121");
+            UserRepository.AddNewUser(UserRoles.TRANSPORTER, "Pablo", "Lamponne",
+                "cuartoLamponne", "NoHaceFaltaSaleSolo", "099212121");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void URepositoryAddNewUserRepeatedMailInvalidTest()
+        {
+            UserRepository.AddNewUser(UserRoles.TRANSPORTER, "Pablo", "Lamponne",
+                "repeatedUsername", "NoHaceFaltaSaleSolo", "099212121");
+            UserRepository.AddNewUser(UserRoles.YARD_OPERATOR, "Gabriel David", "Medina",
+                "repeatedUsername", "MusicaSuperDivertida", "096869689");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryAddNewUserInvalidFirstNameTest()
+        {
+            UserRepository.AddNewUser(UserRoles.YARD_OPERATOR, "1d2@#!9 #(", "Medina",
+                "gdMedina", "MusicaSuperDivertida", "096869689");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryAddNewUserInvalidLastNameTest()
+        {
+            UserRepository.AddNewUser(UserRoles.YARD_OPERATOR, "Gabriel David", "*$ 563a%7*//*0&d!@",
+                "gdMedina", "MusicaSuperDivertida", "096869689");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryAddNewUserInvalidUsernameTest()
+        {
+            UserRepository.AddNewUser(UserRoles.YARD_OPERATOR, "Gabriel David", "Medina",
+                "Ceci n'est pas un nom d'utilisateur.", "MusicaSuperDivertida", "096869689");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryAddNewUserInvalidPasswordTest()
+        {
+            UserRepository.AddNewUser(UserRoles.YARD_OPERATOR, "Gabriel David", "Medina",
+                "gdMedina", "  \n \t \t\t\n ", "096869689");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryAddNewUserInvalidPhoneNumberTest()
+        {
+            UserRepository.AddNewUser(UserRoles.YARD_OPERATOR, "Gabriel David", "Medina",
+                "gdMedina", "MusicaSuperDivertida", "La juguetería del Señor Simón.");
+        }
+    }
+}
