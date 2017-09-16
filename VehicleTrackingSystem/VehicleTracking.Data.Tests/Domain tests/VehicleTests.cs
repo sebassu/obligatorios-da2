6fb21317a2469cb7fd433d92eb44c643fd9bf1af
luciadabezies <luciadabezies@hotmail.com>
2017-09-16 15:35:53 -0300

    [Red][Vehicle][Brand]: A new set of unit tests was created, regarding the "Brand" property in class "Vehicle"

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/VehicleTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Tests.Domain_tests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class VehicleTests
+    {
+        private static Vehicle testingVehicle;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            testingVehicle = Vehicle.InstanceForTestingPurposes();
+        }
+
+        //Vehicle Brand
+        [TestMethod]
+        public void VehicleForTestingPurposesTest()
+        {
+            Assert.AreEqual("Audi", testingVehicle.Brand);
+        }
+
+        [TestMethod]
+        public void VehicleSetValidBrandTest()
+        {
+            testingVehicle.Brand = "Fiat";
+            Assert.AreEqual("Fiat", testingVehicle.Brand);
+        }
+
+        [TestMethod]
+        public void VehicleSetValidBrandCompoundTest()
+        {
+            testingVehicle.Brand = "  Mercedes Benz ";
+            Assert.AreEqual("Mercedes Benz", testingVehicle.Brand);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VehicleSetInvalidBrandNumbersTest()
+        {
+            testingVehicle.Brand = "123";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VehicleSetInvalidBrandEmptyTest()
+        {
+            testingVehicle.Brand = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VehicleSetInvalidBrandOnlySpacesTest()
+        {
+            testingVehicle.Brand = "     ";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VehicleSetInvalidBrandNullTest()
+        {
+            testingVehicle.Brand = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VehicleSetInvalidBrandPunctuationTest()
+        {
+            testingVehicle.Brand = "!@$#%^";
+        }
+    }
+}
