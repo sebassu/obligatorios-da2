9354c0eee7e9dc286c0e61d8dc9096677bccb779
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-09-17 10:19:20 -0300

    [Red][UserRepository][Modify]: A series of new unit tests was created, linked to the "Modify" static method in class "UserRepository".

--- VehicleTrackingSystem/VehicleTracking.Data.Tests/Persistence tests/UserRepositoryTests.cs
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Persistence tests/UserRepositoryTests.cs
@@ -167,5 +167,116 @@ namespace Data.Persistence.Tests
                 UserRepository.Remove(administrator);
             }
         }
+
+        [TestMethod]
+        public void URepositoryModifyUserValidTest()
+        {
+            User userToVerify = UserRepository.AddNewUser(UserRoles.YARD_OPERATOR,
+                "Algún tipo", "X", "tipo123", "vuelveLaX", "aaa123");
+            UserRepository.ModifyUser(userToVerify, UserRoles.PORT_OPERATOR, "Gabriel David",
+                "Medina", "gdMedina7", "MúsicaSuperDivertida", "096869689");
+            Assert.AreEqual(UserRoles.PORT_OPERATOR, userToVerify.Role);
+            Assert.AreEqual("Gabriel David", userToVerify.FirstName);
+            Assert.AreEqual("Medina", userToVerify.LastName);
+            Assert.AreEqual("gdMedina7", userToVerify.Username);
+            Assert.AreEqual("MúsicaSuperDivertida", userToVerify.Password);
+            Assert.AreEqual("096869689", userToVerify.PhoneNumber);
+        }
+
+        [TestMethod]
+        public void URepositoryModifyUserSetSameDataValidTest()
+        {
+            User userToVerify = UserRepository.Elements.First();
+            var previousRole = userToVerify.Role;
+            var previousFirstName = userToVerify.FirstName;
+            var previousLastName = userToVerify.LastName;
+            var previousUsername = userToVerify.Username;
+            var previousPassword = userToVerify.Password;
+            var previousPhoneNumber = userToVerify.PhoneNumber;
+            UserRepository.ModifyUser(userToVerify, previousRole, previousFirstName,
+                previousLastName, previousUsername, previousPassword, previousPhoneNumber);
+            Assert.AreEqual(previousRole, userToVerify.Role);
+            Assert.AreEqual(previousFirstName, userToVerify.FirstName);
+            Assert.AreEqual(previousLastName, userToVerify.LastName);
+            Assert.AreEqual(previousUsername, userToVerify.Username);
+            Assert.AreEqual(previousPassword, userToVerify.Password);
+            Assert.AreEqual(previousPhoneNumber, userToVerify.PhoneNumber);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void URepositoryModifyNullUserInvalidTest()
+        {
+            UserRepository.ModifyUser(null, UserRoles.PORT_OPERATOR, "Gabriel David",
+                "Medina", "gdMedina8", "MúsicaSuperDivertida", "096869689");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void URepositoryModifyNotAddedUserInvalidTest()
+        {
+            User notAddedUser = User.CreateNewUser(UserRoles.TRANSPORTER, "Pablo",
+                "Lamponne", "pLamponne5", "NoHaceFaltaSaleSolo", "099212121");
+            UserRepository.ModifyUser(notAddedUser, UserRoles.PORT_OPERATOR, "Gabriel David",
+                "Medina", "gdMedina9", "MúsicaSuperDivertida", "096869689");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryModifyUserInvalidFirstNameTest()
+        {
+            User addedUser = UserRepository.Elements.FirstOrDefault();
+            UserRepository.ModifyUser(addedUser, addedUser.Role, "4%# !sf*!@#9",
+                addedUser.LastName, addedUser.Username, addedUser.Password, addedUser.PhoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryModifyUserInvalidLastNameTest()
+        {
+            User addedUser = UserRepository.Elements.FirstOrDefault();
+            UserRepository.ModifyUser(addedUser, addedUser.Role, addedUser.FirstName,
+                "a#$%s 9 $^!!12", addedUser.Username, addedUser.Password, addedUser.PhoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryModifyUserInvalidUsernameTest()
+        {
+            User addedUser = UserRepository.Elements.FirstOrDefault();
+            UserRepository.ModifyUser(addedUser, addedUser.Role, addedUser.FirstName,
+                addedUser.LastName, "!!12345 6789!!", addedUser.Password, addedUser.PhoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryModifyUserInvalidPasswordTest()
+        {
+            User addedUser = UserRepository.Elements.FirstOrDefault();
+            UserRepository.ModifyUser(addedUser, addedUser.Role, addedUser.FirstName,
+                addedUser.LastName, addedUser.Username, " \t\t\n \n\n  ", addedUser.PhoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UserException))]
+        public void URepositoryModifyUserInvalidPhoneNumberTest()
+        {
+            User addedUser = UserRepository.Elements.FirstOrDefault();
+            UserRepository.ModifyUser(addedUser, addedUser.Role, addedUser.FirstName,
+                addedUser.LastName, addedUser.Username, addedUser.Password, "a &#^ 12&$!!/*- ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void URepositoryModifyUserCausesRepeatedUsernameInvalidTest()
+        {
+            User userToModify = UserRepository.AddNewUser(UserRoles.PORT_OPERATOR, "Gabriel David",
+                "Medina", "gdMedina9", "MúsicaSuperDivertida", "096869689");
+            UserRepository.AddNewUser(UserRoles.PORT_OPERATOR, "Gabriel David",
+                "Medina", "simulador", "MúsicaSuperDivertida", "096869689");
+            UserRepository.ModifyUser(userToModify, UserRoles.ADMINISTRATOR, "Mario",
+                "Santos", "simulador", "DisculpeFuegoTiene", "099424242");
+        }
     }
 }
