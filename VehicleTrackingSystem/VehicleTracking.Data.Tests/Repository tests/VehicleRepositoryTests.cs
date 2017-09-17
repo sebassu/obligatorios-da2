c38bc6f8e6f148492dae1572af0eb9c17404befa
luciadabezies <luciadabezies@hotmail.com>
2017-09-17 10:18:46 -0300

    [Red][VehicleRepository][AddNewVehicle]: A set of new unit tests was created, regarding the "AddNewVehicle" method in class "VehicleRepository".

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Repository tests/VehicleRepositoryTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+using Domain;
+
+namespace Data.Tests.Repository_tests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class VehicleRepositoryTests
+    {
+        [AssemblyInitialize]
+        public static void AssemblySetup(TestContext context)
+        {
+            DeleteAllDatabaseData();
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblySetup()
+        {
+            DeleteAllDatabaseData();
+        }
+
+        private static void DeleteAllDatabaseData()
+        {
+            using (var context = new VTSystemContext())
+            {
+                context.DeleteAllData();
+            }
+        }
+
+        [TestMethod]
+        public void VRepositoryAddNewVehicleValidTest()
+        {
+            Vehicle vehicleToVerify = Vehicle.CreateNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2015, "Red", "ASDFGHJKL12345678");
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2015, "Red", "ASDFGHJKL12345678");
+            CollectionAssert.Contains(VehicleRepository.Elements.ToList(), vehicleToVerify);
+        }
+
+        [TestMethod]
+        public void VRepositoryAddNewVehcleReturnsAddedVehicleValidTest()
+        {
+            Vehicle addedVehicle = VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2015, "Red", "ASDFGHJKL12345678");
+            CollectionAssert.Contains(VehicleReposiory.Elements.ToList(), addedVehicle);
+        }
+
+        [TestMethod]
+        public void VRepositoryAddNewVehicleOnlyVinMatchValidTest()
+        {
+            Vehicle vehicleToVerify = Vehicle.InstanceForTestingPurposes();
+            vehicleToVerify.Vin = "ZXCVBNM1234567890";
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2015, "Red", "ZXCVBNM1234567890");
+            CollectionAssert.Contains(VehicleRepository.Elements.ToList(), vehicleToVerify);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void VRepositoryAddRepeatedVehicleInvalidTest()
+        {
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2015, "Red", "ZXCVBNM1234567890");
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2015, "Red", "ZXCVBNM1234567890");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void VRepositoryAddNewVehicleRepeatedVinInvalidTest()
+        {
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2015, "Red", "ZXCVBNM1234567890");
+            VehicleRepository.AddNewUser(VehicleType.CAR, "Audi", "A1",
+                2017, "Black", "ZXCVBNM1234567890");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryAddNewVehicleInvalidBrandTest()
+        {
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "¡Chevrolet!", "Onix",
+                2015, "Red", "ZXCVBNM1234567890");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryAddNewVehicleInvalidModelTest()
+        {
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix#",
+                2015, "Red", "ZXCVBNM1234567890");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryAddNewVehicleInvalidYearTest()
+        {
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2030, "Red", "ZXCVBNM1234567890");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryAddNewVehicleInvalidColorTest()
+        {
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2016, "!@#", "ZXCVBNM1234567890");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryAddNewVehicleInvalidVinTest()
+        {
+            VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2016, "Red", "ZXCVBNM");
+        }
+    }
+}
