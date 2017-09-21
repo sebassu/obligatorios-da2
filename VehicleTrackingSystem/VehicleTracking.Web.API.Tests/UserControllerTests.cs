6148c6fdcb177704cb9f52049f7ded9e74e5e007
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-09-21 09:17:08 -0300

    [Red][UsersController][AddNewUserFromDTO]: A set of new unit tests was created, regarding the "AddNewUserFromDTO" method in class "UsersController".

--- VehicleTrackingSystem/VehicleTracking.Web.API.Tests/UserControllerTests.cs
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Tests/UserControllerTests.cs
@@ -20,7 +20,7 @@ namespace Web.API.Tests
             var mockUserServices = new Mock<IUsersServices>();
             mockUserServices.Setup(u => u.GetAllUsers()).Returns(expectedUsers);
             var controller = new UsersController(mockUserServices.Object);
-            IHttpActionResult obtainedResult = controller.Get();
+            IHttpActionResult obtainedResult = controller.GetRegisteredUsers();
             var contentResult = obtainedResult as
                 OkNegotiatedContentResult<IReadOnlyCollection<UserDTO>>;
             mockUserServices.VerifyAll();
@@ -49,7 +49,7 @@ namespace Web.API.Tests
             var mockUserServices = new Mock<IUsersServices>();
             mockUserServices.Setup(u => u.GetAllUsers()).Returns(expectedUsers);
             var controller = new UsersController(mockUserServices.Object);
-            IHttpActionResult obtainedResult = controller.Get();
+            IHttpActionResult obtainedResult = controller.GetRegisteredUsers();
             var contentResult = obtainedResult as
                 OkNegotiatedContentResult<IReadOnlyCollection<UserDTO>>;
             mockUserServices.VerifyAll();
@@ -66,10 +66,43 @@ namespace Web.API.Tests
             var mockUserServices = new Mock<IUsersServices>();
             mockUserServices.Setup(u => u.GetAllUsers()).Returns(unexpectedUsers);
             var controller = new UsersController(mockUserServices.Object);
-            IHttpActionResult obtainedResult = controller.Get();
+            IHttpActionResult obtainedResult = controller.GetRegisteredUsers();
             mockUserServices.VerifyAll();
             Assert.IsNotNull(obtainedResult);
             Assert.IsInstanceOfType(obtainedResult, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public void UControllerAddValidNewUserOkTest()
+        {
+            var fakeUser = UserDTO.FromUser(User.CreateNewUser(UserRoles.ADMINISTRATOR, "Mario", "Santos",
+                "mSantos", "DisculpeFuegoTiene", "099424242"));
+            fakeUser.Id = 42;
+            var mockUserServices = new Mock<IUsersServices>();
+            mockUserServices.Setup(u => u.Add(fakeUser)).Returns(fakeUser.Id);
+            var controller = new UsersController(mockUserServices.Object);
+            IHttpActionResult obtainedResult = controller.AddNewUserFromDTO(fakeUser);
+            var result = obtainedResult as CreatedAtRouteNegotiatedContentResult<UserDTO>;
+            mockUserServices.VerifyAll();
+            Assert.IsNotNull(result);
+            Assert.AreEqual("DefaultApi", result.RouteName);
+            Assert.AreEqual(fakeUser.Id, result.RouteValues["id"]);
+            Assert.AreEqual(fakeUser, result.Content);
+        }
+
+        [TestMethod]
+        public void UControllerCreateNullUserBadRequestTest()
+        {
+            var expectedErrorMessage = "Some error message";
+            var mockUserServices = new Mock<IUsersServices>();
+            mockUserServices.Setup(u => u.Add(null)).Throws(
+                new VTSystemException(expectedErrorMessage));
+            var controller = new UsersController(mockUserServices.Object);
+            IHttpActionResult obtainedResult = controller.AddNewUserFromDTO(null);
+            var result = obtainedResult as BadRequestErrorMessageResult;
+            mockUserServices.VerifyAll();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedErrorMessage, result.Message);
         }
     }
 }
