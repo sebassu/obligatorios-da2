092a8165aa4b9441aace66ab6a21dbb19ea1634c
luciadabezies <luciadabezies@hotmail.com>
2017-09-21 12:19:13 -0300

    [Green][Location][Name]: The code needed to make the previously added unit tests pass.

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Entities/Location.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Location
+    {
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (IsValidName(value))
+                {
+                    name = value.Trim();
+                }
+                else
+                {
+                    string errorMessage = string.Format(CultureInfo.CurrentCulture,
+                        ErrorMessages.NameIsInvalid, "Nombre", value);
+                    throw new LocationException(errorMessage);
+                }
+            }
+        }
+
+        protected virtual bool IsValidName(string value)
+        {
+            return Utilities.ContainsLettersOrSpacesOnly(value);
+        }
+
+
+        internal static Location InstanceForTestingPurposes()
+        {
+            return new Location();
+        }
+
+        protected Location()
+        {
+            name = "Location name";
+        }
+        }
+}
--- VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/LocationTests.cs
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/LocationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
 
 namespace Data.Tests.Domain_tests
 {
