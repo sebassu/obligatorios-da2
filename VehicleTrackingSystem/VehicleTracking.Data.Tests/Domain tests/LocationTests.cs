2de4d8e39bb7203e64425a69c4a71626f543af35
luciadabezies <luciadabezies@hotmail.com>
2017-09-21 12:10:51 -0300

    [Red][Location][Name]: A set of new unit tests was created, regarding the "Name" property in class "Location".

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/LocationTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Tests.Domain_tests
+{
+    [TestClass]
+    public class LocationTests
+    {
+        private static Location testingLocation;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            testingLocation = Location.InstanceForTestingPurposes();
+        }
+
+        //Location Name
+        [TestMethod]
+        public void LocationForTestingPurposesTest()
+        {
+            Assert.AreEqual("Location name", testingLocation.Name);
+        }
+
+        [TestMethod]
+        public void LocationSetValidNameTest()
+        {
+            testingLocation.Name = "Another name";
+            Assert.AreEqual("Another name", testingLocation.Name);
+        }
+
+        [TestMethod]
+        public void LocationSetValidNameCompoundTest()
+        {
+            testingLocation.Name = "  Some place ";
+            Assert.AreEqual("Some place", testingLocation.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LocationException))]
+        public void LocationSetInvalidNameNumbersTest()
+        {
+            testingLocation.Name = "12345";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LocationException))]
+        public void LocationSetInvalidNameEmptyTest()
+        {
+            testingLocation.Name = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LocationException))]
+        public void LocationSetInvalidNameOnlySpacesTest()
+        {
+            testingLocation.Name = "     ";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LocationException))]
+        public void LocationSetInvalidNameNullTest()
+        {
+            testingLocation.Name = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LocationException))]
+        public void LocationSetInvalidNamePunctuationTest()
+        {
+            testingLocation.Name = "!@$#%^";
+        }
+    }
+}
