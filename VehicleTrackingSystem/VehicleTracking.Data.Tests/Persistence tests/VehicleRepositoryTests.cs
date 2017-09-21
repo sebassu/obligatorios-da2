16d0f4ba75cd3f630cd886fc3b0f76809b674af4
luciadabezies <luciadabezies@hotmail.com>
2017-09-20 21:17:18 -0300

    [Red][VehicleRepository][ModifyVehicle]: A set of new unit tests was created, regarding the "ModifyVehicle" method in class "VehicleRepository".

--- VehicleTrackingSystem/VehicleTracking.Data.Tests/Persistence tests/VehicleRepositoryTests.cs
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Persistence tests/VehicleRepositoryTests.cs
@@ -125,5 +125,117 @@ namespace Data.Persistence.Tests
         {
             VehicleRepository.Remove(null);
         }
+
+        //Modify
+        [TestMethod]
+        public void VRepositoryModifyVehicleValidTest()
+        {
+            Vehicle vehicleToModify = VehicleRepository.AddNewVehicle(VehicleType.CAR, "Chevrolet", "Onix",
+                2015, "Red", "1234567890AQSWDEF");
+            VehicleRepository.ModifyVehicle(vehicleToModify, VehicleType.SUV, "Suzuki", "Vitara",
+                2016, "Grey", "0987654321POIUYTR");
+            Assert.AreEqual(VehicleType.SUV, vehicleToModify.Type);
+            Assert.AreEqual("Suzuki", vehicleToModify.Brand);
+            Assert.AreEqual("Vitara", vehicleToModify.Model);
+            Assert.AreEqual(2016, vehicleToModify.Year);
+            Assert.AreEqual("Grey", vehicleToModify.Color);
+            Assert.AreEqual("0987654321POIUYTR", vehicleToModify.Vin);
+        }
+
+        [TestMethod]
+        public void VRepositoryModifyVehicleSetSameDataValidTest()
+        {
+            Vehicle vehicleToVerify = VehicleRepository.Elements.First();
+            var prevoiusType = vehicleToVerify.Type;
+            var previousBrand = vehicleToVerify.Brand;
+            var previousModel = vehicleToVerify.Model;
+            var prevoiusYear = vehicleToVerify.Year;
+            var previousColor = vehicleToVerify.Color;
+            var prevoiusVin = vehicleToVerify.Vin;
+            VehicleRepository.ModifyVehicle(vehicleToVerify, prevoiusType, previousBrand,
+                previousModel, prevoiusYear, previousColor, prevoiusVin);
+            Assert.AreEqual(prevoiusType, vehicleToVerify.Type);
+            Assert.AreEqual(previousBrand, vehicleToVerify.Brand);
+            Assert.AreEqual(previousModel, vehicleToVerify.Model);
+            Assert.AreEqual(prevoiusYear, vehicleToVerify.Year);
+            Assert.AreEqual(previousColor, vehicleToVerify.Color);
+            Assert.AreEqual(prevoiusVin, vehicleToVerify.Vin);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void VRepositoryModifyNullVehicleInvalidTest()
+        {
+            VehicleRepository.ModifyVehicle(null, VehicleType.SUV, "Suzuki", "Vitara",
+                2016, "Grey", "0987654321POIUYTR");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void VRepositoryModifyNotAddedVehicleInvalidTest()
+        {
+            Vehicle notAddedVehicle = Vehicle.CreateNewVehicle(VehicleType.SUV, "Suzuki", "Vitara",
+                2016, "Grey", "P0O9I8U76T5R4E3W2");
+            VehicleRepository.ModifyVehicle(notAddedVehicle, VehicleType.SUV, "Suzuki", "Vitara",
+                2016, "Grey", "0987654321POIUYTR");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryModifyVehicleInvalidBrandTest()
+        {
+            Vehicle addedVehicle = VehicleRepository.Elements.FirstOrDefault();
+            VehicleRepository.ModifyVehicle(addedVehicle, addedVehicle.Type, "12345#", addedVehicle.Model,
+                addedVehicle.Year, addedVehicle.Color, addedVehicle.Vin);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryModifyVehicleInvalidModelTest()
+        {
+            Vehicle addedVehicle = VehicleRepository.Elements.FirstOrDefault();
+            VehicleRepository.ModifyVehicle(addedVehicle, addedVehicle.Type, addedVehicle.Brand, "!@#$%",
+                addedVehicle.Year, addedVehicle.Color, addedVehicle.Vin);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryModifyVehicleInvalidYearTest()
+        {
+            Vehicle addedVehicle = VehicleRepository.Elements.FirstOrDefault();
+            VehicleRepository.ModifyVehicle(addedVehicle, addedVehicle.Type, addedVehicle.Brand, addedVehicle.Model,
+                2222, addedVehicle.Color, addedVehicle.Vin);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryModifyVehicleInvalidColorTest()
+        {
+            Vehicle addedVehicle = VehicleRepository.Elements.FirstOrDefault();
+            VehicleException.ModifyVehicle(addedVehicle, addedVehicle.Type, addedVehicle.Brand, addedVehicle.Model,
+                addedVehicle.Year, "!@#$%", addedVehicle.Vin);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleException))]
+        public void VRepositoryModifyVehicleInvalidVinTest()
+        {
+            Vehicle addedVehicle = VehicleRepository.Elements.FirstOrDefault();
+            VehicleRepository.ModifyVehicle(addedVehicle, addedVehicle.Type, addedVehicle.Brand, addedVehicle.Model,
+                addedVehicle.Year, addedVehicle.Color, "1234567");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void VRepositoryModifyVehicleCausesRepeatedVinInvalidTest()
+        {
+            Vehicle vehicleToModify = VehicleRepository.AddNewVehicle(VehicleType.SUV, "Suzuki", "Vitara",
+                2016, "Grey", "0987654321POIUBNM");
+            VehicleRepository.AddNewVehicle(VehicleType.SUV, "Audi", "A1",
+                2016, "Red", "0987654321POIÑLKJ");
+            VehicleRepository.ModifyVehicle(vehicleToModify, VehicleType.SUV, "Suzuki", "Vitara",
+                2016, "Grey", "0987654321POIÑLKJ");
+        }
     }
 }
