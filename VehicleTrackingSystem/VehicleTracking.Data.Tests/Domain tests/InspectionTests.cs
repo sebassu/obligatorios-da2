07ca085c7aa18fc97a8efa4fbe1ab789a25ceb38
luciadabezies <luciadabezies@hotmail.com>
2017-09-22 10:50:34 -0300

    [Red][Inspection][DateTime]: A set of new unit tests was created regarding the "DateTime" property in class "Inspection"

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/InspectionTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain;
+
+namespace Data.Tests.Domain_tests
+{
+    [TestClass]
+    public class InspectionTests
+    {
+        private static Inspection testingInspection;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            testingInspection = Inspection.InstanceForTestingPurposes();
+
+        }
+
+        [TestMethod]
+        public void VehicleForTestingPurposesTest()
+        {
+            Assert.AreEqual(0, testingInspection.Id);
+            Assert.AreEqual(new DateTime(2017, 9, 22, 10, 8, 0), testingInspection.DateTime);
+        }
+
+        [TestMethod]
+        public void InspectionSetValidDateTimeTest()
+        {
+            testingInspection.DateTime = DateTime.Now;
+            Assert.AreEqual(DateTime.Now, testingInspection.DateTime);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InspectionException))]
+        public void InspectionSetInvalidDatePassTodayTest()
+        {
+            testingInspection.DateTime = new DateTime(2019, 9, 24, 10, 9, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InspectionException))]
+        public void InspectionSetInvalidDateTimeOldTest()
+        {
+            testingInspection.DateTime = new DateTime(1856, 8, 30, 12, 8, 9);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InspectionException))]
+        public void InspectionSetInvalidDateTimeEmptyTest()
+        {
+            testingInspection.DateTime = null;
+        }
+    }
+}
