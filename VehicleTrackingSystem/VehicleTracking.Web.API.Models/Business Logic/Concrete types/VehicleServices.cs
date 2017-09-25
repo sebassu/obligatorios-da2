6657392f86b5a37df031e33d18ca1a9c7cd97235
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-09-25 11:01:28 -0300

    [Green][VehicleServices][GetRegisteredVehicles]: The code needed to make the previously added unit tests pass is introduced.

---
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Models/Business Logic/Concrete types/VehicleServices.cs
@@ -0,0 +1,46 @@
+﻿using Persistence;
+using System;
+using System.Collections.Generic;
+
+namespace API.Services
+{
+    public class VehicleServices : IVehicleServices
+    {
+        internal IVehicleRepository Model { get; }
+
+        public VehicleServices(IVehicleRepository someRepository = null)
+        {
+            Model = someRepository;
+        }
+
+        public IEnumerable<VehicleDTO> GetRegisteredVehicles()
+        {
+            var result = new List<VehicleDTO>();
+            foreach (var vehicle in Model.Elements)
+            {
+                result.Add(VehicleDTO.FromVehicle(vehicle));
+            }
+            return result.AsReadOnly();
+        }
+
+        public int AddNewVehicleFromData(VehicleDTO vehicleToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public VehicleDTO GetVehicleWithVIN(string vinToLookup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifyVehicleWithVIN(string vinToModify, VehicleDTO vehicleDataToSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveVehicleWithVIN(string vinToRemove)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
