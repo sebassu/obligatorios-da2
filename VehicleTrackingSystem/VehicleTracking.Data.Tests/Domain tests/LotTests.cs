93b64e52675fd31c21ff829ef9de16de50b63947
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-09-30 10:30:06 -0300

    [Red][Lot][Description]: A series of new unit tests was added, regarding the "Description" property in class "Lot".

--- VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/LotTests.cs
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/LotTests.cs
@@ -18,6 +18,7 @@ namespace Data.Tests.Domain_tests
         public void LotInstanceForTestingPurposesTest()
         {
             Assert.AreEqual("Lote inválido", testingLot.Name);
+            Assert.AreEqual("Descripción inválida", testingLot.Description);
         }
 
         [TestMethod]
@@ -67,6 +68,48 @@ namespace Data.Tests.Domain_tests
         public void LotSetInvalidNameNullTest()
         {
             testingLot.Name = null;
+        }
+
+        [TestMethod]
+        public void LotSetValidDescriptionTest()
+        {
+            testingLot.Description = "Lote de nuevos ingresos a puerto.";
+            Assert.AreEqual("Lote de nuevos ingresos a puerto.", testingLot.Description);
+        }
+
+        [TestMethod]
+        public void LotSetValidDescriptionNumbersTest()
+        {
+            testingLot.Description = "134564654654";
+            Assert.AreEqual("134564654654", testingLot.Description);
+        }
+
+        [TestMethod]
+        public void LotSetValidDescriptionPunctuationTest()
+        {
+            testingLot.Description = "$/% --#$%&/!!-./";
+            Assert.AreEqual("$/% --#$%&/!!-./", testingLot.Description);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LotException))]
+        public void LotSetInvalidDescriptionEmptyTest()
+        {
+            testingLot.Description = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LotException))]
+        public void LotSetInvalidDescriptionOnlySpacesTest()
+        {
+            testingLot.Description = "     ";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(LotException))]
+        public void LotSetInvalidDescriptionNullTest()
+        {
+            testingLot.Description = null;
         }
     }
 }
