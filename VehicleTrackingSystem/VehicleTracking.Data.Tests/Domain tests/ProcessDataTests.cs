d8f02a6d7f67874388c14f12eca15cd21014c36b
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-10-01 10:51:31 -0300

    [Red][ProcessData][RegisterPortLot]: A series of new unit tests was created, regarding the "RegisterPortLot" setter method in class "ProcessData".

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/ProcessDataTests.cs
@@ -0,0 +1,49 @@
+﻿using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Tests.Domain_tests
+{
+    [TestClass]
+    public class ProcessDataTests
+    {
+        private static ProcessData testingData;
+        private static readonly Lot lotToSet = Lot.InstanceForTestingPurposes();
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            testingData = new ProcessData();
+        }
+
+        [TestMethod]
+        public void ProcessDataRegisterPortLotValidTest()
+        {
+            Assert.AreEqual(ProcessStages.PORT, testingData.CurrentStage);
+            testingData.RegisterPortLot(lotToSet);
+            Assert.AreEqual(lotToSet, testingData.PortLot);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ProcessException))]
+        public void ProcessDataSetNullPortLotInvalidTest()
+        {
+            testingData.RegisterPortLot(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ProcessException))]
+        public void ProcessDataRegisterPortLotOnTransportStageInvalidTest()
+        {
+            testingData.CurrentStage = ProcessStages.TRANSPORT;
+            testingData.RegisterPortLot(lotToSet);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ProcessException))]
+        public void ProcessDataRegisterPortLotOnYardStageInvalidTest()
+        {
+            testingData.CurrentStage = ProcessStages.YARD;
+            testingData.RegisterPortLot(lotToSet);
+        }
+    }
+}
