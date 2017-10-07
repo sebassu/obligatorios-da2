61b1a41113ac1445595d16d513564b41a996aaf9
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-10-07 18:44:28 -0300

    [TransportRepository]: The "TransportRepository" class was added. Also, classes in the DataAccess project were organized into folders.
    Which is bound to cause git issues.

---
+++ VehicleTrackingSystem/VehicleTracking.Data.DataAccess/TransportRepository/ITransportRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+
+namespace Persistence
+{
+    public interface ITransportRepository
+    {
+        void AddNewTransport(Transport transportToAdd);
+        Transport GetTransportWithId(int idToLookup);
+        void UpdateTransport(Transport transportToModify);
+    }
+}
---
+++ VehicleTrackingSystem/VehicleTracking.Data.DataAccess/TransportRepository/TransportRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Domain;
+using System.Linq;
+using System.Globalization;
+
+namespace Persistence
+{
+    internal class TransportRepository : GenericRepository<Transport>, ITransportRepository
+    {
+        public TransportRepository(VTSystemContext someContext)
+            : base(someContext) { }
+
+        public void AddNewTransport(Transport transportToAdd)
+        {
+            Add(transportToAdd);
+        }
+
+        public Transport GetTransportWithId(int idToLookup)
+        {
+            try
+            {
+                return elements.Include("Lots.Vehicles.StagesData")
+                    .Single(s => s.Id == idToLookup);
+            }
+            catch (InvalidOperationException)
+            {
+                string errorMessage = string.Format(CultureInfo.CurrentCulture,
+                    ErrorMessages.CouldNotFindField, "identificador de transporte", idToLookup);
+                throw new RepositoryException(errorMessage);
+            }
+        }
+
+        public void UpdateTransport(Transport transportToModify)
+        {
+            Update(transportToModify);
+        }
+
+        protected override bool ElementExistsInCollection(Transport entityToUpdate)
+        {
+            return Utilities.IsNotNull(entityToUpdate)
+                && elements.Any(t => t.Id == entityToUpdate.Id);
+        }
+    }
+}
