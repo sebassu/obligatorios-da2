cb576cf3055206876eb73ac0003970d69ff8744b
luciadabezies <luciadabezies@hotmail.com>
2017-10-07 13:42:10 -0300

    [LotServices][LotDTO]: LotDTO was created and some methods where introduced

---
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Models/Data Transfer Objects/LotDTO.cs
@@ -0,0 +1,50 @@
+﻿using Domain;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Services.Data_Transfer_Objects
+{
+    class LotDTO
+    {
+        public int Id { get; set; }
+        public string CreatorName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<string> VehicleVINs { get; set; }
+
+        internal LotDTO() { }
+
+        internal static LotDTO FromLot(Lot someLot)
+        {
+            return new LotDTO(someLot);
+        }
+
+        protected LotDTO(Lot someLot) : this(someLot.Id,
+            someLot.Name, someLot.Description)
+        {
+            SetVehiclesIds(someLot);
+        }
+
+        private void SetVehiclesIds(Lot someLot)
+        {
+            var vehiclesToSet = someLot.Vehicles;
+            if (Utilities.IsNotNull(vehiclesToSet))
+            {
+                VehicleVINs = vehiclesToSet.Select(v => v.VIN).ToList();
+            }
+        }
+
+        protected LotDTO(int idToSet, string nameToSet,
+            string descriptionToSet)
+        {
+            Id = idToSet;
+            Name = nameToSet;
+            Description = descriptionToSet;
+        }
+
+        internal Lot ToLot(User creator, ICollection<Vehicle> vehicles)
+        {
+            return Lot.CreatorNameDescriptionVehicles(creator, Name, Description, vehicles);
+        }
+    }
+}
