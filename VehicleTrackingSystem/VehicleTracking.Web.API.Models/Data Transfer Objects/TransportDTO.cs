95c7ba32d5a3a73c09b2fd3d93baf7cf2c2a90f4
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-10-07 19:15:22 -0300

    [TransportDTO]: A first approach at the "TransportDTO" class is made.

---
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Models/Data Transfer Objects/TransportDTO.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace API.Services
+{
+    public class TransportDTO
+    {
+        public string TransporterUsername { get; set; }
+
+        [Required]
+        public DateTime StartDateTime { get; set; }
+
+        [Required]
+        public ICollection<string> TransportedLotsNames { get; set; }
+
+        public DateTime? EndDateTime { get; set; }
+
+        internal static TransportDTO FromTransport(Transport someTransport)
+        {
+            return new TransportDTO(someTransport);
+        }
+
+        public TransportDTO(Transport someTransport)
+        {
+            TransporterUsername = someTransport.Transporter.Username;
+            StartDateTime = someTransport.StartDateTime.Value;
+            TransportedLotsNames = someTransport.LotsTransported
+                .Select(l => l.ToString()).ToList();
+            EndDateTime = someTransport.EndDateTime;
+        }
+    }
+}
