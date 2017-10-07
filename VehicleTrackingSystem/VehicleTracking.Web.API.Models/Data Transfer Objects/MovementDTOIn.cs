38318872e7f5e2d9a79a985a1083f234a6923b3d
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-10-07 16:45:29 -0300

    [MovementDTO]: Creation of two separate DTOs for entity "Movement", given the vehicle history requirements to be satisfied.

---
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Models/Data Transfer Objects/MovementDTOIn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Services
+{
+    public class MovementDTOIn
+    {
+        public string ResponsibleUsername { get; set; }
+
+        [Required]
+        public int DepartureSubzoneId { get; set; }
+
+        [Required]
+        public int ArrivalSubzoneId { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+    }
+}
