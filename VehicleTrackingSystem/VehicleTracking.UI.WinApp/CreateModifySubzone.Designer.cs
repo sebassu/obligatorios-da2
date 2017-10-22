a1bbf80b9adf5736b81aff7c5416efac0f68f909
luciadabezies <luciadabezies@hotmail.com>
2017-10-22 13:05:27 -0300

    CreateModifySubzone was created

---
+++ VehicleTrackingSystem/VehicleTracking.UI.WinApp/CreateModifySubzone.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace VehicleTracking.UI.WinApp
+{
+    partial class CreateModifySubzone
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.TitleLbl = new System.Windows.Forms.Label();
+            this.ZoneLbl = new System.Windows.Forms.Label();
+            this.CapacityTxt = new System.Windows.Forms.TextBox();
+            this.CapacityLbl = new System.Windows.Forms.Label();
+            this.NameTxt = new System.Windows.Forms.TextBox();
+            this.NameLbl = new System.Windows.Forms.Label();
+            this.ZoneComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.OkBtn.FlatAppearance.BorderSize = 6;
+            this.OkBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OkBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OkBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.OkBtn.Location = new System.Drawing.Point(288, 269);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(119, 46);
+            this.OkBtn.TabIndex = 25;
+            this.OkBtn.Text = "Button 1";
+            this.OkBtn.UseVisualStyleBackColor = true;
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.CancelBtn.FlatAppearance.BorderSize = 6;
+            this.CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.CancelBtn.Location = new System.Drawing.Point(161, 269);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(117, 46);
+            this.CancelBtn.TabIndex = 24;
+            this.CancelBtn.Text = "Cancelar";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // TitleLbl
+            // 
+            this.TitleLbl.AutoSize = true;
+            this.TitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.TitleLbl.Location = new System.Drawing.Point(252, 75);
+            this.TitleLbl.Name = "TitleLbl";
+            this.TitleLbl.Size = new System.Drawing.Size(109, 39);
+            this.TitleLbl.TabIndex = 23;
+            this.TitleLbl.Text = "label1";
+            // 
+            // ZoneLbl
+            // 
+            this.ZoneLbl.AutoSize = true;
+            this.ZoneLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZoneLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.ZoneLbl.Location = new System.Drawing.Point(148, 216);
+            this.ZoneLbl.Name = "ZoneLbl";
+            this.ZoneLbl.Size = new System.Drawing.Size(59, 24);
+            this.ZoneLbl.TabIndex = 17;
+            this.ZoneLbl.Text = "Zona:";
+            // 
+            // CapacityTxt
+            // 
+            this.CapacityTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CapacityTxt.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.CapacityTxt.Location = new System.Drawing.Point(259, 173);
+            this.CapacityTxt.Name = "CapacityTxt";
+            this.CapacityTxt.Size = new System.Drawing.Size(150, 26);
+            this.CapacityTxt.TabIndex = 16;
+            // 
+            // CapacityLbl
+            // 
+            this.CapacityLbl.AutoSize = true;
+            this.CapacityLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CapacityLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.CapacityLbl.Location = new System.Drawing.Point(148, 175);
+            this.CapacityLbl.Name = "CapacityLbl";
+            this.CapacityLbl.Size = new System.Drawing.Size(105, 24);
+            this.CapacityLbl.TabIndex = 15;
+            this.CapacityLbl.Text = "Capacidad:";
+            // 
+            // NameTxt
+            // 
+            this.NameTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameTxt.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.NameTxt.Location = new System.Drawing.Point(259, 132);
+            this.NameTxt.Name = "NameTxt";
+            this.NameTxt.Size = new System.Drawing.Size(150, 26);
+            this.NameTxt.TabIndex = 14;
+            // 
+            // NameLbl
+            // 
+            this.NameLbl.AutoSize = true;
+            this.NameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.NameLbl.Location = new System.Drawing.Point(148, 134);
+            this.NameLbl.Name = "NameLbl";
+            this.NameLbl.Size = new System.Drawing.Size(84, 24);
+            this.NameLbl.TabIndex = 13;
+            this.NameLbl.Text = "Nombre:";
+            // 
+            // ZoneComboBox
+            // 
+            this.ZoneComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZoneComboBox.FormattingEnabled = true;
+            this.ZoneComboBox.Location = new System.Drawing.Point(259, 217);
+            this.ZoneComboBox.Name = "ZoneComboBox";
+            this.ZoneComboBox.Size = new System.Drawing.Size(148, 28);
+            this.ZoneComboBox.TabIndex = 26;
+            // 
+            // CreateModifySubzone
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.Controls.Add(this.ZoneComboBox);
+            this.Controls.Add(this.OkBtn);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.TitleLbl);
+            this.Controls.Add(this.ZoneLbl);
+            this.Controls.Add(this.CapacityTxt);
+            this.Controls.Add(this.CapacityLbl);
+            this.Controls.Add(this.NameTxt);
+            this.Controls.Add(this.NameLbl);
+            this.Name = "CreateModifySubzone";
+            this.Size = new System.Drawing.Size(573, 378);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OkBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Label TitleLbl;
+        private System.Windows.Forms.Label ZoneLbl;
+        private System.Windows.Forms.TextBox CapacityTxt;
+        private System.Windows.Forms.Label CapacityLbl;
+        private System.Windows.Forms.TextBox NameTxt;
+        private System.Windows.Forms.Label NameLbl;
+        private System.Windows.Forms.ComboBox ZoneComboBox;
+    }
+}
---
+++ VehicleTrackingSystem/VehicleTracking.UI.WinApp/CreateModifySubzone.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using API.Services;
+using Persistence;
+
+namespace VehicleTracking.UI.WinApp
+{
+    public partial class CreateModifySubzone : UserControl
+    {
+        ISubzoneServices SubzoneInstance;
+        IZoneServices ZoneIntance;
+        SubzoneDTO SelectedSubzone;
+        Panel CardPnl;
+        string Origin;
+
+        public CreateModifySubzone(Panel cardPanel, string origin, SubzoneDTO selectedSubzone)
+        {
+            IUnitOfWork unitOfWork = new UnitOfWork();
+            SubzoneInstance = new SubzoneServices(unitOfWork);
+            ZoneIntance = new ZoneServices(unitOfWork);
+            CardPnl = cardPanel;
+            Origin = origin;
+            SelectedSubzone = selectedSubzone;
+            LoadInfo();
+            InitializeComponent();
+        }
+
+        private void LoadInfo()
+        {
+            if (Origin.Equals("modify"))
+            {
+                NameTxt.Text = SelectedSubzone.Name;
+                CapacityTxt.Text = SelectedSubzone.Capacity.ToString();
+                TitleLbl.Text = "Modificar subzona";
+                OkBtn.Text = "Modificar";
+            }else
+            {
+                TitleLbl.Text = "Agregar subzona";
+                OkBtn.Text = "Agregar";
+            }
+        }
+    }
+}
