41d7ffd957278e6dea1c4f7f5f6b2dd6f9f58642
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-10-26 15:34:57 -0300

    [Red][ImageElement][SetImageData]: A series of new unit tests was created, regarding the "ImageData" property in class "ImageElement".

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/ImageElementTests.cs
@@ -0,0 +1,67 @@
+﻿using Domain;
+using System.IO;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Domain_tests
+{
+    [TestClass]
+    public class ImageElementTests
+    {
+        private static ImageElement testingImageElement;
+        private static byte[] testImageBinaryData;
+        private static string testImageLocation = Directory.GetParent(
+            Directory.GetCurrentDirectory()).Parent.FullName + "\\Resources\\TestImage.jpg";
+
+        [ClassInitialize]
+        public static void ClassSetup(TestContext context)
+        {
+            Image testImage = Image.FromFile(testImageLocation);
+            ImageConverter converter = new ImageConverter();
+            testImageBinaryData = converter.ConvertTo(testImage,
+                typeof(byte[])) as byte[];
+        }
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            testingImageElement = ImageElement.InstanceForTestingPurposes();
+        }
+
+        [TestMethod]
+        public void ImageElementSetIdValidTest()
+        {
+            testingImageElement.Id = 42;
+            Assert.AreEqual(42, testingImageElement.Id);
+        }
+
+        [TestMethod]
+        public void ImageElementSetImageDataValidTest()
+        {
+            testingImageElement.ImageData = testImageBinaryData;
+            Assert.AreSame(testImageBinaryData, testingImageElement.ImageData);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleTrackingException))]
+        public void ImageElementSetImageRandomDataInvalidTest()
+        {
+            byte[] someRandomData = new byte[3] { 1, 0, 1 };
+            testingImageElement.ImageData = someRandomData;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleTrackingException))]
+        public void ImageElementSetImageEmptyDataInvalidTest()
+        {
+            testingImageElement.ImageData = new byte[7];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(VehicleTrackingException))]
+        public void ImageElementSetImageNullDataInvalidTest()
+        {
+            testingImageElement.ImageData = null;
+        }
+    }
+}
