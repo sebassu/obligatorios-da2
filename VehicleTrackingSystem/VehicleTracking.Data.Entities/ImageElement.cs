7a145bb64162903ac1f19f5fd0ce15fd53a868a9
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-10-26 15:36:00 -0300

    [Green][ImageElement][SetImageData]: The code needed to make the previously added unit tests pass is introduced.

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Entities/ImageElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace Domain
+{
+    public class ImageElement
+    {
+        public int Id { get; set; }
+
+        private byte[] imageData;
+        public virtual byte[] ImageData
+        {
+            get { return imageData; }
+            set
+            {
+                try
+                {
+                    AttemptToSetData(value);
+                }
+                catch (ArgumentException)
+                {
+                    throw new VehicleTrackingException(
+                        ErrorMessages.BinaryInformationIsNotAnImage);
+                }
+            }
+        }
+
+        private void AttemptToSetData(byte[] value)
+        {
+            using (var memoryStream = new MemoryStream(value))
+            {
+                Image.FromStream(memoryStream);
+                imageData = value;
+            }
+        }
+
+        internal static ImageElement InstanceForTestingPurposes()
+        {
+            return new ImageElement();
+        }
+
+        protected ImageElement() { }
+    }
+}
