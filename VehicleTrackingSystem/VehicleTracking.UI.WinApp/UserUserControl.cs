961a8b9f324eedc40d9bc5740314a5426262bc93
luciadabezies <luciadabezies@hotmail.com>
2017-10-29 11:59:47 -0300

    UserUserControl was created

---
+++ VehicleTrackingSystem/VehicleTracking.UI.WinApp/UserUserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehicleTracking.UI.WinApp
+{
+    public partial class UserUserControl : UserControl
+    {
+        public UserUserControl(Panel cardPanel)
+        {
+            InitializeComponent();
+        }
+    }
+}
