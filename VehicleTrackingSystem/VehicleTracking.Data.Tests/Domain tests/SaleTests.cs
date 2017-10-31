ba82d5458597abdc8ce2e9ee5770d1df877fb6f7
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-10-31 16:23:29 -0300

    [Red][Sale][VehicleVIN]: A series of new unit tests is added, regarding the "VehicleVIN" property in class "Sale".

--- VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/SaleTests.cs
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/SaleTests.cs
@@ -55,5 +55,54 @@ namespace Data.Domain_tests
         {
             testingSale.SellingPrice = -2112;
         }
+
+        [TestMethod]
+        public void SaleSetValidVehicleVINTest()
+        {
+            testingSale.VehicleVIN = "RUSH2112RLLTHEBNS";
+            Assert.AreEqual("RUSH2112RLLTHEBNS", testingSale.VehicleVIN);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SaleException))]
+        public void SaleSetInvalidVehicleVINTooShortTest()
+        {
+            testingSale.VehicleVIN = "QWERT12345";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SaleException))]
+        public void SaleSetInvalidVehicleVINTooLongTest()
+        {
+            testingSale.VehicleVIN = "QWERTY1234567890QWERTY";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SaleException))]
+        public void SaleSetInvalidVehicleVINPunctuationTest()
+        {
+            testingSale.VehicleVIN = "09&,. #%$*%%@!!--**//";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SaleException))]
+        public void SaleSetInvalidVehicleVINSpacesTest()
+        {
+            testingSale.VehicleVIN = "  \n   \n\t\t \n   ";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SaleException))]
+        public void SaleSetInvalidVehicleVINEmptyTest()
+        {
+            testingSale.VehicleVIN = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SaleException))]
+        public void SaleSetNullVehicleVINInvalidTest()
+        {
+            testingSale.VehicleVIN = null;
+        }
     }
 }
