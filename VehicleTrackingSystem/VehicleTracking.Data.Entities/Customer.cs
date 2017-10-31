a45196a1289269c9d36ae7735a89b3b36bfd1f67
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-10-31 14:14:29 -0300

    [Green][Customer][Name]: The code needed for the previously added unit tests to pass was introduced.

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Entities/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace Domain
+{
+    public class Customer
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (IsValidName(value))
+                {
+                    name = value.Trim();
+                }
+                else
+                {
+                    string errorMessage = string.Format(CultureInfo.CurrentCulture,
+                        ErrorMessages.NameIsInvalid, "Nombre", value);
+                    throw new CustomerException(errorMessage);
+                }
+            }
+        }
+
+        protected bool IsValidName(string value)
+        {
+            return Utilities.ContainsLettersOrSpacesOnly(value);
+        }
+
+        internal static Customer InstanceForTestingPurposes()
+        {
+            return new Customer();
+        }
+
+        protected Customer()
+        {
+            name = "Cliente inválido";
+        }
+    }
+}
