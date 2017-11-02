777437cae8de440037a0293ef724ad1c494ec0f8
luciadabezies <luciadabezies@hotmail.com>
2017-11-02 11:08:33 -0300

    SaleFlowUserControl appearance changed

--- VehicleTrackingSystem/VehicleTracking.UI.WinApp/SaleFlowUserControl.Designer.cs
+++ VehicleTrackingSystem/VehicleTracking.UI.WinApp/SaleFlowUserControl.Designer.cs
@@ -28,10 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.TitleLbl = new System.Windows.Forms.Label();
+            this.RemoveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(178, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(198, 199);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(507, 95);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(198, 199);
+            this.listBox2.TabIndex = 1;
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.FlatAppearance.BorderSize = 0;
+            this.AddBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddBtn.Font = new System.Drawing.Font("Wingdings 3", 54.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.AddBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.AddBtn.Location = new System.Drawing.Point(403, 103);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(96, 85);
+            this.AddBtn.TabIndex = 2;
+            this.AddBtn.Text = "]";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            // 
+            // TitleLbl
+            // 
+            this.TitleLbl.AutoSize = true;
+            this.TitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.TitleLbl.Location = new System.Drawing.Point(134, 25);
+            this.TitleLbl.Name = "TitleLbl";
+            this.TitleLbl.Size = new System.Drawing.Size(647, 39);
+            this.TitleLbl.TabIndex = 4;
+            this.TitleLbl.Text = "Definir el flujo para la venta de vehiculos:";
+            // 
+            // RemoveBtn
+            // 
+            this.RemoveBtn.FlatAppearance.BorderSize = 0;
+            this.RemoveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RemoveBtn.Font = new System.Drawing.Font("Wingdings 3", 54.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.RemoveBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.RemoveBtn.Location = new System.Drawing.Point(403, 194);
+            this.RemoveBtn.Name = "RemoveBtn";
+            this.RemoveBtn.Size = new System.Drawing.Size(96, 85);
+            this.RemoveBtn.TabIndex = 5;
+            this.RemoveBtn.Text = "\\";
+            this.RemoveBtn.UseVisualStyleBackColor = true;
+            // 
+            // SaleFlowUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.Controls.Add(this.RemoveBtn);
+            this.Controls.Add(this.TitleLbl);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "SaleFlowUserControl";
+            this.Size = new System.Drawing.Size(901, 365);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Label TitleLbl;
+        private System.Windows.Forms.Button RemoveBtn;
     }
 }
