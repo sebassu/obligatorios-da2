de9d206af822bd60da49edc1268cb67fa73bb7d7
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-11-02 21:26:12 -0300

    [Red][Flow][EncodedSubzoneNames]: A series of new unit tests was introduced, regarding the "EncodedSubzoneNames" property in class "Flow".

--- VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/FlowTests.cs
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Domain tests/FlowTests.cs
@@ -1,6 +1,7 @@
 ﻿using Domain;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Data.Domain_tests
 {
@@ -8,7 +9,7 @@ namespace Data.Domain_tests
     public class FlowTests
     {
         private static Flow testingFlow;
-        private static readonly IEnumerable<string> testingSubzoneNames
+        private static readonly List<string> testingSubzoneNames
             = new List<string>() { "A", "B", "C" };
 
         [TestInitialize]
@@ -34,6 +35,15 @@ namespace Data.Domain_tests
 
         [TestMethod]
         [ExpectedException(typeof(FlowException))]
+        public void FlowSetRequiredSubzonesWithInvalidContentsValidTest()
+        {
+            var subzoneNamesWithInvalidContents = new List<string>() { "Valid subzone",
+                "12#&$^$^#^ ***-,1" };
+            testingFlow.RequiredSubzoneNames = subzoneNamesWithInvalidContents;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FlowException))]
         public void FlowSetRequiredSubzonesEmptyEnumerationInvalidTest()
         {
             testingFlow.RequiredSubzoneNames = new List<string>();
@@ -50,7 +60,8 @@ namespace Data.Domain_tests
         public void FlowParameterFactoryMethodValidTest()
         {
             testingFlow = Flow.FromSubzoneNames(testingSubzoneNames);
-            Assert.AreSame(testingSubzoneNames, testingFlow.RequiredSubzoneNames);
+            CollectionAssert.AreEqual(testingSubzoneNames,
+                testingFlow.RequiredSubzoneNames.ToList());
             Assert.AreEqual("A,B,C", testingFlow.EncodedSubzoneNames);
         }
 
@@ -59,8 +70,18 @@ namespace Data.Domain_tests
         {
             var subzoneNamesWithDuplicates = new List<string>() { "A", "A" };
             testingFlow = Flow.FromSubzoneNames(subzoneNamesWithDuplicates);
-            Assert.AreSame(subzoneNamesWithDuplicates, testingFlow.RequiredSubzoneNames);
+            CollectionAssert.AreEqual(subzoneNamesWithDuplicates,
+                testingFlow.RequiredSubzoneNames.ToList());
             Assert.AreEqual("A,A", testingFlow.EncodedSubzoneNames);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FlowException))]
+        public void FlowParameterFactoryMethodWithInvalidContentTest()
+        {
+            var subzoneNamesWithInvalidContents = new List<string>() {
+                "49 !&^!!_-+>> ,*#", "Valid subzone" };
+            Flow.FromSubzoneNames(subzoneNamesWithInvalidContents);
         }
 
         [TestMethod]
@@ -75,6 +96,43 @@ namespace Data.Domain_tests
         public void FlowParameterFactoryMethodNullEnumerationInvalidTest()
         {
             testingFlow.RequiredSubzoneNames = null;
+        }
+
+        [TestMethod]
+        public void FlowSetEncodedSubzoneNamesValidTest()
+        {
+            testingFlow.EncodedSubzoneNames = "A,B,C";
+            Assert.AreEqual("A,B,C", testingFlow.EncodedSubzoneNames);
+            CollectionAssert.AreEqual(testingSubzoneNames.ToList(),
+                testingFlow.RequiredSubzoneNames.ToList());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FlowException))]
+        public void FlowSetEncodedSubzoneNamesStringWithEmptySpacesInvalidTest()
+        {
+            testingFlow.EncodedSubzoneNames = ",Valid subzone,,";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FlowException))]
+        public void FlowSetEncodedSubzoneNamesWithRandomDataInvalidTest()
+        {
+            testingFlow.EncodedSubzoneNames = "Valid subzone, )as!!/$/%$-*";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FlowException))]
+        public void FlowSetEmptyEncodedSubzoneNamesInvalidTest()
+        {
+            testingFlow.EncodedSubzoneNames = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FlowException))]
+        public void FlowSetNullEncodedSubzoneNamesInvalidTest()
+        {
+            testingFlow.EncodedSubzoneNames = null;
         }
     }
 }
