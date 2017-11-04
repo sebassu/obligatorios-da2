e7e09873aeb41241e8eee4e0b87142ea40021c85
luciadabezies <luciadabezies@hotmail.com>
2017-11-04 11:57:18 -0300

    [Refactor][SessionServices][LogIn]

--- VehicleTrackingSystem/VehicleTracking.Web.API.Models/Business Logic/Concrete types/SessionServices.cs
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Models/Business Logic/Concrete types/SessionServices.cs
@@ -15,35 +15,54 @@ namespace API.Services
 
         public static bool LogIn(string username, string password)
         {
-            User actual;
             if (username.Trim() != "" && password.Trim() != "")
             {
-                using (IUnitOfWork unitOfWork = new UnitOfWork())
-                {
-                    IUserRepository users = unitOfWork.Users;
-                    actual = users.GetUserWithUsername(username);
-                }
-                if (actual.Password.Equals(password))
-                {
-                    if (actual.Role.Equals(UserRoles.ADMINISTRATOR))
-                    {
-                        LoggedUser = actual;
-                        return true;
-                    }
-                    else
-                    {
-                        throw new ServiceException(ErrorMessages.WrongUserRole);
-                    }
-                }
-                else
-                {
-                    throw new ServiceException(ErrorMessages.NotMatchingPasswords);
-                }
+                return AttemptToLogIn(username, password);
             }
             else
             {
                 throw new ServiceException(ErrorMessages.NotEmptyValues);
             }
+        }
+
+        private static bool AttemptToLogIn(string username, string password)
+        {
+            User actualUser = GetUser(username);
+            if (ValidPassword(password, actualUser))
+            {
+                if (ValidateRole(actualUser))
+                {
+                    LoggedUser = actualUser;
+                    return true;
+                }
+                else
+                {
+                    throw new ServiceException(ErrorMessages.WrongUserRole);
+                }
+            }
+            else
+            {
+                throw new ServiceException(ErrorMessages.NotMatchingPasswords);
+            }
+        }
+
+        private static User GetUser(string username)
+        {
+            using (IUnitOfWork unitOfWork = new UnitOfWork())
+            {
+                IUserRepository users = unitOfWork.Users;
+                return users.GetUserWithUsername(username);
+            }
+        }
+
+        private static bool ValidPassword (string password, User actualUser)
+        {
+            return actualUser.Password.Equals(password);
+        }
+
+        private static bool ValidateRole(User actualUser)
+        {
+            return actualUser.Role.Equals(UserRoles.ADMINISTRATOR);
         }
     }
 }
