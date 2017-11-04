c303d4bd1ae0994353301830a4c78e62108a3ce0
luciadabezies <luciadabezies@hotmail.com>
2017-11-04 17:10:41 -0300

    Type property was added in CreateModifyVehicle

--- VehicleTrackingSystem/VehicleTracking.UI.WinApp/CreateModifyVehicle.Designer.cs
+++ VehicleTrackingSystem/VehicleTracking.UI.WinApp/CreateModifyVehicle.Designer.cs
@@ -42,6 +42,8 @@
             this.VINTxt = new System.Windows.Forms.TextBox();
             this.VINLbl = new System.Windows.Forms.Label();
             this.ImportBtn = new System.Windows.Forms.Button();
+            this.TypeLbl = new System.Windows.Forms.Label();
+            this.TypeComboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // OkBtn
@@ -51,7 +53,7 @@
             this.OkBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.OkBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OkBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.OkBtn.Location = new System.Drawing.Point(457, 296);
+            this.OkBtn.Location = new System.Drawing.Point(457, 313);
             this.OkBtn.Name = "OkBtn";
             this.OkBtn.Size = new System.Drawing.Size(119, 46);
             this.OkBtn.TabIndex = 25;
@@ -66,7 +68,7 @@
             this.CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CancelBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.CancelBtn.Location = new System.Drawing.Point(330, 296);
+            this.CancelBtn.Location = new System.Drawing.Point(330, 313);
             this.CancelBtn.Name = "CancelBtn";
             this.CancelBtn.Size = new System.Drawing.Size(117, 46);
             this.CancelBtn.TabIndex = 24;
@@ -89,7 +91,7 @@
             // 
             this.YearTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.YearTxt.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.YearTxt.Location = new System.Drawing.Point(403, 251);
+            this.YearTxt.Location = new System.Drawing.Point(403, 268);
             this.YearTxt.Name = "YearTxt";
             this.YearTxt.Size = new System.Drawing.Size(187, 26);
             this.YearTxt.TabIndex = 22;
@@ -101,7 +103,7 @@
             this.YearLbl.AutoSize = true;
             this.YearLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.YearLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.YearLbl.Location = new System.Drawing.Point(312, 251);
+            this.YearLbl.Location = new System.Drawing.Point(312, 268);
             this.YearLbl.Name = "YearLbl";
             this.YearLbl.Size = new System.Drawing.Size(50, 24);
             this.YearLbl.TabIndex = 21;
@@ -111,7 +113,7 @@
             // 
             this.ColorTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ColorTxt.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.ColorTxt.Location = new System.Drawing.Point(403, 211);
+            this.ColorTxt.Location = new System.Drawing.Point(403, 228);
             this.ColorTxt.Name = "ColorTxt";
             this.ColorTxt.Size = new System.Drawing.Size(187, 26);
             this.ColorTxt.TabIndex = 20;
@@ -122,7 +124,7 @@
             this.ColorLbl.AutoSize = true;
             this.ColorLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ColorLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.ColorLbl.Location = new System.Drawing.Point(312, 215);
+            this.ColorLbl.Location = new System.Drawing.Point(312, 232);
             this.ColorLbl.Name = "ColorLbl";
             this.ColorLbl.Size = new System.Drawing.Size(60, 24);
             this.ColorLbl.TabIndex = 19;
@@ -132,7 +134,7 @@
             // 
             this.ModelTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ModelTxt.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.ModelTxt.Location = new System.Drawing.Point(404, 169);
+            this.ModelTxt.Location = new System.Drawing.Point(404, 186);
             this.ModelTxt.Name = "ModelTxt";
             this.ModelTxt.Size = new System.Drawing.Size(186, 26);
             this.ModelTxt.TabIndex = 18;
@@ -143,7 +145,7 @@
             this.ModelLbl.AutoSize = true;
             this.ModelLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ModelLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.ModelLbl.Location = new System.Drawing.Point(312, 171);
+            this.ModelLbl.Location = new System.Drawing.Point(312, 188);
             this.ModelLbl.Name = "ModelLbl";
             this.ModelLbl.Size = new System.Drawing.Size(79, 24);
             this.ModelLbl.TabIndex = 17;
@@ -153,7 +155,7 @@
             // 
             this.BrandTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BrandTxt.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.BrandTxt.Location = new System.Drawing.Point(404, 128);
+            this.BrandTxt.Location = new System.Drawing.Point(404, 145);
             this.BrandTxt.Name = "BrandTxt";
             this.BrandTxt.Size = new System.Drawing.Size(186, 26);
             this.BrandTxt.TabIndex = 16;
@@ -164,7 +166,7 @@
             this.BrandLbl.AutoSize = true;
             this.BrandLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BrandLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.BrandLbl.Location = new System.Drawing.Point(312, 130);
+            this.BrandLbl.Location = new System.Drawing.Point(312, 147);
             this.BrandLbl.Name = "BrandLbl";
             this.BrandLbl.Size = new System.Drawing.Size(67, 24);
             this.BrandLbl.TabIndex = 15;
@@ -174,7 +176,7 @@
             // 
             this.VINTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.VINTxt.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.VINTxt.Location = new System.Drawing.Point(404, 87);
+            this.VINTxt.Location = new System.Drawing.Point(404, 104);
             this.VINTxt.Name = "VINTxt";
             this.VINTxt.Size = new System.Drawing.Size(186, 26);
             this.VINTxt.TabIndex = 14;
@@ -185,7 +187,7 @@
             this.VINLbl.AutoSize = true;
             this.VINLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.VINLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.VINLbl.Location = new System.Drawing.Point(312, 89);
+            this.VINLbl.Location = new System.Drawing.Point(312, 106);
             this.VINLbl.Name = "VINLbl";
             this.VINLbl.Size = new System.Drawing.Size(46, 24);
             this.VINLbl.TabIndex = 13;
@@ -198,7 +200,7 @@
             this.ImportBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ImportBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ImportBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.ImportBtn.Location = new System.Drawing.Point(608, 33);
+            this.ImportBtn.Location = new System.Drawing.Point(608, 17);
             this.ImportBtn.Name = "ImportBtn";
             this.ImportBtn.Size = new System.Drawing.Size(119, 46);
             this.ImportBtn.TabIndex = 26;
@@ -206,11 +208,33 @@
             this.ImportBtn.UseVisualStyleBackColor = true;
             this.ImportBtn.Visible = false;
             // 
+            // TypeLbl
+            // 
+            this.TypeLbl.AutoSize = true;
+            this.TypeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TypeLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.TypeLbl.Location = new System.Drawing.Point(313, 67);
+            this.TypeLbl.Name = "TypeLbl";
+            this.TypeLbl.Size = new System.Drawing.Size(53, 24);
+            this.TypeLbl.TabIndex = 27;
+            this.TypeLbl.Text = "Tipo:";
+            // 
+            // TypeComboBox
+            // 
+            this.TypeComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TypeComboBox.FormattingEnabled = true;
+            this.TypeComboBox.Location = new System.Drawing.Point(403, 65);
+            this.TypeComboBox.Name = "TypeComboBox";
+            this.TypeComboBox.Size = new System.Drawing.Size(187, 28);
+            this.TypeComboBox.TabIndex = 28;
+            // 
             // CreateModifyVehicle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.Controls.Add(this.TypeComboBox);
+            this.Controls.Add(this.TypeLbl);
             this.Controls.Add(this.ImportBtn);
             this.Controls.Add(this.OkBtn);
             this.Controls.Add(this.CancelBtn);
@@ -248,5 +272,7 @@
         private System.Windows.Forms.TextBox VINTxt;
         private System.Windows.Forms.Label VINLbl;
         private System.Windows.Forms.Button ImportBtn;
+        private System.Windows.Forms.Label TypeLbl;
+        private System.Windows.Forms.ComboBox TypeComboBox;
     }
 }
