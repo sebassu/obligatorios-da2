4c505569f7f40dffc2b8ea4898bdbbc91d03c557
luciadabezies <luciadabezies@hotmail.com>
2017-11-04 09:16:11 -0300

    Some buttons were added in SaleFlowUserControl

--- VehicleTrackingSystem/VehicleTracking.UI.WinApp/SaleFlowUserControl.Designer.cs
+++ VehicleTrackingSystem/VehicleTracking.UI.WinApp/SaleFlowUserControl.Designer.cs
@@ -33,12 +33,14 @@
             this.AddBtn = new System.Windows.Forms.Button();
             this.TitleLbl = new System.Windows.Forms.Label();
             this.RemoveBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(178, 95);
+            this.listBox1.Location = new System.Drawing.Point(178, 83);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(198, 199);
             this.listBox1.TabIndex = 0;
@@ -46,7 +48,7 @@
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(507, 95);
+            this.listBox2.Location = new System.Drawing.Point(507, 83);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(198, 199);
             this.listBox2.TabIndex = 1;
@@ -57,7 +59,7 @@
             this.AddBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.AddBtn.Font = new System.Drawing.Font("Wingdings 3", 54.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.AddBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.AddBtn.Location = new System.Drawing.Point(403, 103);
+            this.AddBtn.Location = new System.Drawing.Point(403, 91);
             this.AddBtn.Name = "AddBtn";
             this.AddBtn.Size = new System.Drawing.Size(96, 85);
             this.AddBtn.TabIndex = 2;
@@ -69,7 +71,7 @@
             this.TitleLbl.AutoSize = true;
             this.TitleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TitleLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.TitleLbl.Location = new System.Drawing.Point(134, 25);
+            this.TitleLbl.Location = new System.Drawing.Point(134, 13);
             this.TitleLbl.Name = "TitleLbl";
             this.TitleLbl.Size = new System.Drawing.Size(647, 39);
             this.TitleLbl.TabIndex = 4;
@@ -81,18 +83,48 @@
             this.RemoveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.RemoveBtn.Font = new System.Drawing.Font("Wingdings 3", 54.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
             this.RemoveBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
-            this.RemoveBtn.Location = new System.Drawing.Point(403, 194);
+            this.RemoveBtn.Location = new System.Drawing.Point(403, 182);
             this.RemoveBtn.Name = "RemoveBtn";
             this.RemoveBtn.Size = new System.Drawing.Size(96, 85);
             this.RemoveBtn.TabIndex = 5;
             this.RemoveBtn.Text = "\\";
             this.RemoveBtn.UseVisualStyleBackColor = true;
             // 
+            // CancelBtn
+            // 
+            this.CancelBtn.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.CancelBtn.FlatAppearance.BorderSize = 6;
+            this.CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.CancelBtn.Location = new System.Drawing.Point(323, 288);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(117, 46);
+            this.CancelBtn.TabIndex = 38;
+            this.CancelBtn.Text = "Cancelar";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.SaveBtn.FlatAppearance.BorderSize = 6;
+            this.SaveBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(205)))), ((int)(((byte)(0)))));
+            this.SaveBtn.Location = new System.Drawing.Point(458, 288);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(117, 46);
+            this.SaveBtn.TabIndex = 39;
+            this.SaveBtn.Text = "Guardar";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            // 
             // SaleFlowUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.CancelBtn);
             this.Controls.Add(this.RemoveBtn);
             this.Controls.Add(this.TitleLbl);
             this.Controls.Add(this.AddBtn);
@@ -112,5 +144,7 @@
         private System.Windows.Forms.Button AddBtn;
         private System.Windows.Forms.Label TitleLbl;
         private System.Windows.Forms.Button RemoveBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Button SaveBtn;
     }
 }
