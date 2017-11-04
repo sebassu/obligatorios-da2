15720f9ec4764dec1868876209b1890c7723290d
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2017-11-04 09:52:03 -0300

    [Red][CustomerRepository][AddNewCustomer]: A series of new unit tests was added, regarding the "AddNewCustomer" method in class "CustomerRepository".

---
+++ VehicleTrackingSystem/VehicleTracking.Data.Tests/Persistence tests/CustomerRepositoryTests.cs
@@ -0,0 +1,64 @@
+﻿using Domain;
+using Persistence;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data.Persistence_tests
+{
+    [TestClass]
+    public class CustomerRepositoryTests
+    {
+        private static readonly IUnitOfWork testingUnitOfWork = new UnitOfWork();
+        private static ICustomerRepository testingCustomerRepository;
+
+        [ClassInitialize]
+        public static void ClassSetup(TestContext context)
+        {
+            testingCustomerRepository = testingUnitOfWork.Customers;
+            Assert.IsNotNull(testingCustomerRepository);
+        }
+
+        #region AddNewCustomer tests
+        [TestMethod]
+        public void CRepositoryAddNewCustomerValidTest()
+        {
+            var customerToAdd = Customer.InstanceForTestingPurposes();
+            AddNewCustomerAndSaveChanges(customerToAdd);
+            CollectionAssert.Contains(testingCustomerRepository
+                .Elements.ToList(), customerToAdd);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RepositoryException))]
+        public void CRepositoryAddNullCustomerInvalidTest()
+        {
+            AddNewCustomerAndSaveChanges(null);
+        }
+        #endregion
+
+        #region Default ElementExistsInCollection tests
+        [TestMethod]
+        public void CRepositoryElementExistsInCollectionExistingElementTest()
+        {
+            var addedCustomer = Customer.InstanceForTestingPurposes();
+            AddNewCustomerAndSaveChanges(addedCustomer);
+            var castRepostory = testingCustomerRepository as GenericRepository<Customer>;
+            Assert.IsFalse(castRepostory.ElementExistsInCollection(addedCustomer));
+        }
+
+        [TestMethod]
+        public void CRepositoryElementExistsInCollectionUnaddedElementTest()
+        {
+            var unaddedCustomer = Customer.InstanceForTestingPurposes();
+            var castRepostory = testingCustomerRepository as GenericRepository<Customer>;
+            Assert.IsFalse(castRepostory.ElementExistsInCollection(unaddedCustomer));
+        }
+        #endregion
+
+        private void AddNewCustomerAndSaveChanges(Customer customerToAdd)
+        {
+            testingCustomerRepository.AddNewCustomer(customerToAdd);
+            testingUnitOfWork.SaveChanges();
+        }
+    }
+}
