1ec175b82e17bb118e4f0b3ad794f2e36c15c2db
luciadabezies <luciadabezies@hotmail.com>
2017-11-04 11:48:49 -0300

    [Green][SessionServices][LogIn]: The code needed to make the previously introduced unit tests pass.

---
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Models/Business Logic/Concrete types/SessionServices.cs
@@ -0,0 +1,51 @@
+﻿using Domain;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public static class SessionServices
+    {
+
+        public static User LoggedUser;
+
+        public static bool LogIn(string username, string password)
+        {
+            User actual;
+            if (username.Trim() != "" && password.Trim() != "")
+            {
+                using (IUnitOfWork unitOfWork = new UnitOfWork())
+                {
+                    IUserRepository users = unitOfWork.Users;
+                    actual = users.GetUserWithUsername(username);
+                }
+                if (actual.Password.Equals(password))
+                {
+                    if (actual.Role.Equals(UserRoles.ADMINISTRATOR))
+                    {
+                        LoggedUser = actual;
+                        return true;
+                    }
+                    else
+                    {
+                        throw new ServiceException(ErrorMessages.WrongUserRole);
+                    }
+                }
+                else
+                {
+                    throw new ServiceException(ErrorMessages.NotMatchingPasswords);
+                }
+            }
+            else
+            {
+                throw new ServiceException(ErrorMessages.NotEmptyValues);
+            }
+        }
+    }
+}
+    
+
