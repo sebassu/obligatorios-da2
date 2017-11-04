69077e5f1ccf80591979c6aa4b47bff26b360043
luciadabezies <luciadabezies@hotmail.com>
2017-11-04 15:29:48 -0300

    [Red][FlowServices][AddNewFlowFromData]: A series of new unit tests was added, regarding the "AddNewFlowFromData" method in class "FlowServices".

---
+++ VehicleTrackingSystem/VehicleTracking.Web.API.Tests/Services Tests/FlowServicesTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics.CodeAnalysis;
+using Persistence;
+using Moq;
+using Domain;
+using API.Services;
+
+namespace Web.API.Tests.Services_Tests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class FlowServicesTests
+    {
+        private static readonly FlowServices testingFlowServices = new FlowServices();
+        private static readonly List<string> testingFlowData = new List<string>(new string[] {"Subzone 1", "Subzone 2", "Subzone 3" });
+
+        [TestMethod]
+        public void FServicesDefaultParameterlessConstructorTest()
+        {
+            Assert.IsNotNull(testingFlowServices.Model);
+        }
+        
+        [TestMethod]
+        public void UServicesAddNewFlowFromDataValidTest()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(f => f.Flow.RegisterNewFlow(It.IsAny<Flow>()))
+                .Verifiable();
+            var flowServices = new FlowServices(mockUnitOfWork.Object);
+            flowServices.AddNewFlowFromData(testingFlowData);
+            mockUnitOfWork.Verify();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ServiceException))]
+        public void FServicesAddNewFlowFromNullDataInvalidTest()
+        {
+            testingFlowServices.AddNewFlowFromData(null);
+        }
+    }
+}
