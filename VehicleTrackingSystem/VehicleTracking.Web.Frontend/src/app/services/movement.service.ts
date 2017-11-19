d2f541b5a3069b8338aa6ad2a5375bb871ae11c3
luciadabezies <luciadabezies@hotmail.com>
2017-11-19 10:20:49 -0300

    movement.service was created and registerNewMovement was implemented

---
+++ VehicleTrackingSystem/VehicleTracking.Web.Frontend/src/app/services/movement.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers, Response } from '@angular/http';
+import { environment } from '../../environments/environment';
+import { MovementIn } from '../entities/movement-in';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { BaseService } from './base.service';
+
+@Injectable()
+export class MovementService extends BaseService {
+
+    private static URL: string = environment.APIURL + "/api/Vehicles/";
+
+    constructor(_httpService: Http) { super(_httpService); }
+
+    registerNewMovement(arrival: number) {
+        let header = this.getHeader();
+        let body = { "DateTime": new Date(), "ArrivalSubzoneId": arrival }
+        return this._httpService.post(MovementService.URL, body, { 'headers': header })
+            .map((response: Response) => alert("Movimiento registrado correctamente."))
+            .catch(this.handleError);
+    }
+}
